28c16023efd4ca746ed9448d3552fe282f1e4884
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2025-02-07 00:00:45 +0700

    up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\PaketTryout;
 use App\Models\UserTryouts;
+use App\Observers\UserAccessPaketObserver;
 use App\Observers\UserTryoutsObserver;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         UserTryouts::observe(UserTryoutsObserver::class);
+        PaketTryout::observe(UserAccessPaketObserver::class);
     }
 }
