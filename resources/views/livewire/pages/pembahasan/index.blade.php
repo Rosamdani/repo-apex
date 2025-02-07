04500490392bb3eac385b95ae73ff5c903efc344
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2025-02-08 03:31:12 +0700

    up

--- resources/views/livewire/pages/pembahasan/index.blade.php
+++ resources/views/livewire/pages/pembahasan/index.blade.php
@@ -317,7 +317,6 @@ new class extends Component {
         <div class="card radius-8 border-0 mb-3">
             <div class="card-body">
                 <div class="grid justify-content-center grid-cols-1 g-2 mb-2">
-                    @dd($questions)
                     @foreach ($questions as $index => $question)
                         @php
                             $buttonClass =
