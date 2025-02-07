766a4351d6d3318c07f5a827fa25ed072af8cff0
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2025-02-07 23:58:43 +0700

    fix bug

--- resources/views/livewire/pages/katalog/detail.blade.php
+++ resources/views/livewire/pages/katalog/detail.blade.php
@@ -262,42 +262,44 @@ new class extends Component {
             </div>
         </div>
     </div>
-    <div wire:ignore.self class="modal fade" id="konfirmasiAdminModal" tabindex="-1"
-        aria-labelledby="konfirmasiAdminModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="konfirmasiAdminModalLabel">Konfirmasi Admin</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    @if (session()->has('message'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('message') }}
-                        </div>
-                    @endif
-                    <p>{{ $tryout->nama }}?</p>
-                    <div class="mb-3">
-                        <label for="file-upload-name">Upload {{ $this->configurableText() }} </label>
-                        <input type="file" class="form-control w-auto mt-24 form-control-lg" id="file-upload-name"
-                            wire:model="image" accept="image/*">
-                        <ul id="uploaded-img-names"></ul>
-                        @error('image')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
+    @if (!$isRequested)
+        <div wire:ignore.self class="modal fade" id="konfirmasiAdminModal" tabindex="-1"
+            aria-labelledby="konfirmasiAdminModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="konfirmasiAdminModalLabel">Konfirmasi Admin</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                    <button type="button" class="btn btn-primary" wire:click="konfirmasiAdmin"
-                        wire:loading.attr="disabled">
-                        <span wire:loading wire:target="konfirmasiAdmin">Memproses...</span>
-                        <span wire:loading.remove wire:target="konfirmasiAdmin">Kirim Bukti Pembayaran</span>
-                    </button>
+                    <div class="modal-body">
+                        @if (session()->has('message'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('message') }}
+                            </div>
+                        @endif
+                        <p>{{ $tryout->nama }}?</p>
+                        <div class="mb-3">
+                            <label for="file-upload-name">Upload {{ $this->configurableText() }} </label>
+                            <input type="file" class="form-control w-auto mt-24 form-control-lg"
+                                id="file-upload-name" wire:model="image" accept="image/*">
+                            <ul id="uploaded-img-names"></ul>
+                            @error('image')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="button" class="btn btn-primary" wire:click="konfirmasiAdmin"
+                            wire:loading.attr="disabled">
+                            <span wire:loading wire:target="konfirmasiAdmin">Memproses...</span>
+                            <span wire:loading.remove wire:target="konfirmasiAdmin">Kirim Bukti Pembayaran</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endif
 
 
 </div>
